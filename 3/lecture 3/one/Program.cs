c37f04a2901b06b51ada7f452810e8d4c786cdb7
VikkMoor <moorvikk@gmail.com>
2024-08-02 21:40:48 +0500

    lec3 task1 ready

---
+++ 3/lecture 3/one/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿// string Meth(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++) result = result + text;
+//     return result;
+// }
+// string res = Meth(10, "z");
+// Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
