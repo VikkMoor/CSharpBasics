0d9011de41ec6624d2a2dc426d5706d763ac8d99
VikkMoor <moorvikk@gmail.com>
2024-08-04 09:32:27 +0500

    hw3 task1 ready

---
+++ 3/homework/one/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿// Input: 5-digit number, output: palindrome or not.
+Console.Write("Enter 5-digit number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10000 || N > 99999) Console.WriteLine("Please, enter correct number ;P");
+else
+{
+    int[] digit = new int[5];
+    int divisor = 10000;
+    for (int i = 0; i < 5; i++)
+    {
+        digit[i] = (N / divisor) % 10;
+        divisor = divisor / 10;
+    }
+    if (digit[0] == digit[4] && digit[1] == digit[3]) Console.Write("Palindrome");
+    else Console.Write("Not palindrome");
+}
