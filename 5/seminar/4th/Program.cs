db955f28e90769b9ad9e00d42edc6392ca433fc0
VikkMoor <moorvikk@gmail.com>
2024-08-04 11:12:13 +0500

    sem5 task4 ready

---
+++ 5/seminar/4th/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿//123 elements array, output - how many numbers from [10;99]
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+void FillArray(int[] arr1)
+{
+    int len1 = arr1.Length;
+    int pos1 = 0;
+    while (pos1 < len1)
+    {
+        arr1[pos1] = new Random().Next(0, 201);
+        pos1++;
+    }
+}
+void PrintArray(int[] arr2)
+{
+    int len2 = arr2.Length;
+    int pos2 = 0;
+    while (pos2 < len2)
+    {
+        Console.Write($"{arr2[pos2]} ");
+        pos2++;
+    }
+}
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < array.Length; i++) if (array[i] > 9 && array[i] < 100) count++;
+Console.Write(count);
