2b3422d8ba7158b690918d9dd87621d531cc7283
VikkMoor <moorvikk@gmail.com>
2024-08-04 19:24:04 +0500

    sem6 task3 ready

---
+++ 6/seminar 6/3rd/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    int pos = 0;
+    while (pos < len)
+    {
+        Console.Write($"{arr[pos]} ");
+        pos++;
+    }
+    Console.WriteLine();
+}
+Console.Write("Enter N: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arrr = new int[size];
+arrr[0] = 0;
+arrr[1] = arrr[2] = 1;
+for (int i =3; i < arrr.Length; i++) arrr[i] = arrr[i - 1] + arrr[i - 2];
+PrintArray(arrr);
