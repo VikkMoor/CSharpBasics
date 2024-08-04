49c2ed76ac44eaf28a9f8b0ae66bf9aa2d6ab57d
VikkMoor <moorvikk@gmail.com>
2024-08-04 09:16:19 +0500

    lec3 task4 ready

---
+++ 3/lecture 3/4th/Program.cs
@@ -0,0 +1,32 @@
+﻿// Output - sorted array from max to min;
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
