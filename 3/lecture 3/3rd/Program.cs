68ab7824e75fa9dff40e182a376006ec19e5b45f
VikkMoor <moorvikk@gmail.com>
2024-08-04 09:11:38 +0500

    lec3 task3 ready

---
+++ 3/lecture 3/3rd/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++) Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
