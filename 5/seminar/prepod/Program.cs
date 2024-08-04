4b8246fc4576b460927aa242f3b623216947b23e
VikkMoor <moorvikk@gmail.com>
2024-08-04 10:11:23 +0500

    sem5 prepod

---
+++ 5/seminar/prepod/Program.cs
@@ -0,0 +1,7 @@
+﻿﻿string s = "1, 2, 3, 4, 5, 6, 7.";
+string[] nums = s.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+int[] n = nums.Select(Int32.Parse).ToArray();
+foreach (var num in n)
+{
+    Console.WriteLine(num);
+}
