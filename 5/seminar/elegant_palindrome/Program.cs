1bae78481c4d78ff399d8ed7180384f0eb72b0af
VikkMoor <moorvikk@gmail.com>
2024-08-04 10:26:55 +0500

    sem5 elegant palindrome

---
+++ 5/seminar/elegant_palindrome/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿int i, rem = 0, newnum = 0;
+Console.Write("Enter a number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+for (i = input; i > 0; i = (i / 10))
+{
+    rem = i % 10;
+    newnum = (newnum * 10) + rem;
+}
+if (newnum == input) Console.WriteLine("Palindrome");
+else Console.WriteLine("Not palindrome");
