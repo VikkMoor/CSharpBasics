8c13e86b37125f0597fb4d3dbb5f2066f34bda1b
VikkMoor <moorvikk@gmail.com>
2024-08-04 11:00:15 +0500

    sem5 task2 ready

---
+++ 5/seminar/2nd/Program.cs
@@ -0,0 +1,32 @@
+﻿﻿// Negative to positive and versa.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void FillArray(int[] arr1)
+{
+    int len1 = arr1.Length;
+    int pos1 = 0;
+    while (pos1 < len1)
+    {
+        arr1[pos1] = new Random().Next(-10, 11);
+        pos1++;
+    }
+}
+
+void PrintArray(int[] arr2)
+{
+    int len2 = arr2.Length;
+    int pos2 = 0;
+    while (pos2 < len2)
+    {
+        Console.Write($"{arr2[pos2]}, ");
+        pos2++;
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
+int len = array.Length;
+for (int i = 0; i < len; i++) array[i] = array[i] * (-1);
+PrintArray(array);
