df36b6bef058c7f982819b7249e91d0f06df181a
VikkMoor <moorvikk@gmail.com>
2024-08-05 23:13:50 +0500

    sem7 task1 ready

---
+++ 7/one/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-100, 101) / 10.0;
+        }
+    }
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = new double[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
