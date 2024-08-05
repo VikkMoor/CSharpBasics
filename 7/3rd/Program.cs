c42ad91f63acad2391e699346c122cff185fed1e
VikkMoor <moorvikk@gmail.com>
2024-08-05 23:35:24 +0500

    sem7 task3 ready

---
+++ 7/3rd/Program.cs
@@ -0,0 +1,40 @@
+﻿﻿void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ColumnAverage(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        double result = 0, averageSum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            result += matr[i, j];
+        }
+        averageSum = Math.Round(result / matr.GetLength(0), 1);
+        Console.Write($"{averageSum}  ");
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ColumnAverage(matrix);
