5485e7d70ffce07ba6e036361c1ec07a190992d2
VikkMoor <moorvikk@gmail.com>
2024-08-06 22:15:09 +0500

    hw9 task5 ready

---
+++ final hw 9/4th/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(Sum(num));
+
+ int Sum(int N)
+{
+    if (N % 10 == 0) return N;
+    return (N % 10) + Sum(N / 10);
+}
