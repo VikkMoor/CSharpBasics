64a560e952fbf7f3e9b9694b68a9064738cc7928
VikkMoor <moorvikk@gmail.com>
2024-08-06 21:58:28 +0500

    hw9 task1 ready

---
+++ final hw 9/one/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Numbers(1, n));
+
+string Numbers(int i, int n)
+{
+    if (i <= n) return (Numbers(i + 1, n) + $"{i} ");
+    else return String.Empty;
+}
