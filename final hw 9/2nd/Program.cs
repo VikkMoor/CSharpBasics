2f8198885137dcb105607efae439a566cad92c1d
VikkMoor <moorvikk@gmail.com>
2024-08-06 22:09:10 +0500

    hw9 task2 ready

---
+++ final hw 9/2nd/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿Console.Write("Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ int Sum(int a, int b)
+{
+    if (a > b) return 0;
+    else return a + Sum(a + 1, b);
+}
+Console.Write(Sum(m, n));
