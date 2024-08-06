c1abebef55ce1c48ab154319e0ca7b96422d6c90
VikkMoor <moorvikk@gmail.com>
2024-08-06 22:18:27 +0500

    hw9 task5 ready

---
+++ final hw 9/5th/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿Console.Write("Enter A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write(Step(a, b));
+
+int Step(int A, int B)
+{
+    if (B == 0) return 1;
+    return A * Step(A, B - 1);
+}
