2ad86047617c3c8066046f8832d94bd25223830d
Victoria Moor <moorvikk@gmail.com>
2024-08-07 10:27:05 +0500

    Update Program.cs

--- 7/one/Program.cs
+++ 7/one/Program.cs
@@ -18,7 +18,7 @@ void PrintArray(double[,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]}" + "\t"); // "\t" to make matrix more symmetrical;
+            Console.Write(matr[i, j] + "\t"); // "\t" to make matrix more symmetrical;
         }
         Console.WriteLine();
     }
